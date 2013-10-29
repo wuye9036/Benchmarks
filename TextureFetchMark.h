b8ed2baf6fa1cac1bad5ddedf875d68c4dd0759e
Ye WU <wuye9036@gmail.com>
2013-10-29 17:24:59 +0800

    Add code for addresser.

--- TextureFetchMark.h
+++ TextureFetchMark.h
@@ -1,19 +1,42 @@
 #pragma once
 
+#include <cstdint>
+#include <utility>
+
 struct LinearAddresser
 {
+    static std::pair<int, int> AlignedSize(int x, int y)
+    {
+        return std::make_pair(x, y);
+    }
 
+    static int Offset(int x, int y, int w, int h)
+    {
+        return y*h+x;
+    }
 };
 
 struct MortonAddresser
 {
+    static std::pair<int, int> AlignedSize(int x, int y);
+    static int Offset(int x, int y, int w, int h);
 };
 
+template <int TileSize>
 struct TiledAddresser
 {
+    static std::pair<int, int> AlignedSize(int x, int y)
+    {
+        return std::make_pair(
+            (x + TileSize - 1) / TileSize * TileSize,
+            (x + TileSize - 1) / TileSize * TileSize,
+    }
+    static int Offset(int x, int y, int w, int h);
 };
 
 template <typename AddresserT, typename ComponentT>
 class Surface
 {
+    int w, h, alignedW, alignedH;
+    ComponentT* pixels;
 };
